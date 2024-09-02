91e514e80dd6dae03d1ce45e11989692d319cf28
dboybkru <48415715+dboybkru@users.noreply.github.com>
2024-09-02 17:06:41 +0200

    Fix local

---
+++ news.list/local/components/example/article_list/lang/ru/.description.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$MESS['EXAMPLE_ARTICLE_LIST_COMPONENT'] = "Список статей";
+$MESS['EXAMPLE_ARTICLE_LIST_COMPONENT_DESCRIPTION'] = "Компонент для отображения списка статей.";
+$MESS['EXAMPLE_ARTICLE_LIST_NAME'] = "Пример компонентов для статей";
+?>
