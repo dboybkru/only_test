3d1eb8acc0c4634fe99c0304261f6a1091f72083
dboybkru <48415715+dboybkru@users.noreply.github.com>
2024-09-25 19:28:25 +0200

    vP

--- local/components/example/form.result.new/lang/ru/.description.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-$MESS["FORM_RESULT_NEW_PATH_ID"] = "local";
-$MESS["FORM_RESULT_NEW_PATH_NAME"] = "Компоненты проекта";
-$MESS["FORM_RESULT_NEW_COMPONENT"] = "Очередная Интеграция с веб-формой";
-$MESS["FORM_RESULT_NEW_COMPONENT_DESCRIPTION"] = "Очередной Компонент для интеграции веб-формы с шаблоном.";
-?>
